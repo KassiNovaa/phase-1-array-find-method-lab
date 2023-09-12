15ea401afd581f98347cd4295e17135f8f10e855
Kassidy Puello <kassidympuello@gmail.com>
2023-09-11 22:48:24 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// code your solution here
+
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+  ]
+  
+// //   const winningYear = superbowlWin(record);
+// //   console.log(winningYear);
+
+  
+//   function superbowlWin(record) {
+//     const winObject = record.find(game => game.result === "W");
+//     if (winObject) {
+//       return winObject.year;
+//     }
+//     else {
+//         return undefined
+//     }
+    
+//   }
+
+//   console.log(superbowlWin(record));
+
+  
+  function game(record){
+    return record.result === "W"
+  }
+
+  function superbowlWin(record){
+    const winObject = record.find(game)
+    if (winObject) {
+        return winObject.year
+    }
+    else {
+        return undefined
+    }
+  }
+
+  console.log(superbowlWin(record));
